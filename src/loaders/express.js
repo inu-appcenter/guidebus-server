11b90db5c7a8a6ca9f6617278f65731dd6df401b
HeeYeonKim98 <wow9896@naver.com>
2021-06-11 21:33:42 +0900

    :sparkles: swagger 추가

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 
+import swagger from "../../swagger.json";
 import routes from "../api/routes";
 
 const Express = (app) => {
@@ -11,6 +13,11 @@ const Express = (app) => {
   app.use(express.urlencoded({ extended: true }));
 
   app.use("/api", routes());
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swagger, { explorer: true })
+  );
 
   app.get("/", (req, res) => {
     res.send("<h1>Guidebus Server</h1>");
